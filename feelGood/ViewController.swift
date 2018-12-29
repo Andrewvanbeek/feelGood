844ed10153c3140017481a10e6364524eae8a709
Andrew Van Beek <andrewvanbeek90gmail.com>
2018-12-28 17:18:36 -0800

    Initial Commit

---
+++ feelGood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  feelGood
+//
+//  Created by Andrew Van Beek on 12/28/18.
+//  Copyright © 2018 Andrew Van Beek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
